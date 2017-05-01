b7b67db7714aa93dbd3fa6cdb39dd71fe4a88261
Sean M Hamlet <seanmhamlet@gmail.com>
2017-05-01 09:43:13 -0400

    Improve getDisplayHour helper function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,3 @@
-const _HOUR_DISPLAY_MAP = [
-    '12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM',
-    '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM',
-]
-
 /**
  * Given a list of events and a date, filter the events down to those that
  * fall on the same day as the date
@@ -57,8 +52,15 @@ export const getDisplayDate = (timestamp) => {
  * @param {number} hour - The hour
  * @returns {string}
  */
-// TODO: Implement using a more programmatic approach instead of map
-export const getDisplayHour = (hour) => _HOUR_DISPLAY_MAP[hour]
+export const getDisplayHour = (hour) => {
+
+    let period = hour < 12 ? 'AM' : 'PM';
+
+    // Convert hour to 12-hour clock
+    let hh = (hour + 11) % 12 + 1;
+
+    return `${hh}${period}`;
+};
 
 /**
  * Given a list of events, returns the event object whose id matches the specified eventId
