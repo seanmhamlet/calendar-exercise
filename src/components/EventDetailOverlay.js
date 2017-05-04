c81a77351f615049e4f76b1dbcc2241fd355ac84
Sean M Hamlet <seanmhamlet@gmail.com>
2017-05-03 23:46:14 -0400

    Clicking outside event details overlay closes overlay

--- src/components/EventDetailOverlay.js
+++ src/components/EventDetailOverlay.js
@@ -13,19 +13,34 @@ export default class EventDetailOverlay extends PureComponent {
 
     componentDidMount() {
         document.body.classList.add('no-scroll');
-        
+
         // Make sure to store bound version so it can be removed later
         this._handleKeyDownBound = this._handleKeyDown.bind(this);
         document.addEventListener('keydown', this._handleKeyDownBound);
+
+        // Make sure to store bound version so it can be removed later
+        this._handleClickBound = this._handleClick.bind(this);
+        document.addEventListener('mousedown', this._handleClickBound);
     }
 
     componentWillUnmount() {
         document.body.classList.remove('no-scroll');
         document.removeEventListener('keydown', this._handleKeyDownBound);
+        document.removeEventListener('mousedown', this._handleClickBound);
     }
 
     _handleKeyDown(e) {
         if (e.keyCode === 27) {
+            this.props.onClose();
+        }
+    }
+
+    _handleClick(e) {
+        let eventOverlayElement = document.getElementsByClassName('event-detail-overlay')[0];
+
+        let clickInsideEventOverlay = eventOverlayElement.contains(e.target);
+
+        if (!clickInsideEventOverlay) {
             this.props.onClose();
         }
     }
@@ -45,8 +60,6 @@ export default class EventDetailOverlay extends PureComponent {
         let displayDateTime = `${displayDate} ${startHourDisplay} - ${endHourDisplay}`;
 
         // TODO: Add appropriate ARIA tags to overlay/dialog
-        // TODO: Support clicking outside of the overlay to close it
-        // TODO: Support clicking ESC to close it
         return (
             <section className="event-detail-overlay">
                 <div className="event-detail-overlay__container">
